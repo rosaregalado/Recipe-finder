ec52adf068d31b7db7dab4878adbbbb949b61c81
Rosa Regalado <rosaregalado101@gmail.com>
2023-02-28 21:01:59 -0800

    added tailwind css

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Open [http://localhost:3000](http://localhost:3000) to view the JSON data.
 
 ### `npm start`
 
-Runs the app in the development mode.\
+Runs the React app in the development mode.\
 Open [http://localhost:3001](http://localhost:3001) to view it in your browser.
 
 The page will reload when you make changes.\
--- src/RecipeFinder.js
+++ src/RecipeFinder.js
@@ -6,16 +6,16 @@ function RecipeFinder() {
   const [recipes, setRecipes] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const handleSearchInputChange = event => {
-    setSearchQuery(event.target.value);
+  const handleSearchInputChange = e => {
+    setSearchQuery(e.target.value);
   };
 
-  const handleCuisineInputChange = event => {
-    setCuisine(event.target.value);
+  const handleCuisineInputChange = e => {
+    setCuisine(e.target.value);
   };
 
-  const handleFormSubmit = async event => {
-    event.preventDefault();
+  const handleFormSubmit = async e => {
+    e.preventDefault();
     setLoading(true);
     let apiUrl = 'http://localhost:3000/recipes?';
     if (searchQuery) {
@@ -36,16 +36,20 @@ function RecipeFinder() {
   };
 
   return (
-    <div>
-      <h1>Recipe Finder</h1>
-      <form onSubmit={handleFormSubmit}>
-        <label>
-          Search for recipes:
-          <input type="text" value={searchQuery} onChange={handleSearchInputChange} />
+    <div className="max-w-screen-xl mx-auto py-9">
+      <h1 className="text-3xl font-bold mb-4 text-orange-500">Recipe Finder</h1>
+      <form onSubmit={handleFormSubmit} className="flex flex-wrap mb-4">
+        <label className="w-full w-1/2 pr-4">
+          <span className="block mb-2 font-bold">Search for recipes:</span>
+          <input 
+            type="text" 
+            value={searchQuery}
+            placeholder="Enter a food name or ingredient"
+            onChange={handleSearchInputChange} className="border border-orange-400 rounded p-2 w-full" />
         </label>
-        <label>
-          Filter by cuisine:
-          <select value={cuisine} onChange={handleCuisineInputChange}>
+        <label className="w-full w-1/2">
+          <span className="block mb-2 font-bold">Filter by cuisine:</span>
+          <select value={cuisine} onChange={handleCuisineInputChange} className="border border-orange-400 rounded p-2 w-full">
             <option value="">Any cuisine</option>
             <option value="chinese">Chinese</option>
             <option value="indian">Indian</option>
@@ -54,18 +58,24 @@ function RecipeFinder() {
             <option value="thai">Thai</option>
           </select>
         </label>
-        <button type="submit">Search</button>
+        <div className="my-5">
+          <button type="submit" className="bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-600">Search</button>
+        </div>
       </form>
       {loading ? (
-        <p>Loading recipes...</p>
+        <p className="text-xl mt-4">Loading recipes...</p>
       ) : (
-        recipes.map(recipe => (
-          <div key={recipe.id}>
-            <h2>{recipe.title}</h2>
-            <img src={`https://spoonacular.com/recipeImages/${recipe.id}-556x370.${recipe.imageType}`} alt={recipe.title} />
-            <p>{recipe.summary}</p>
-          </div>
-        ))
+        <div className="grid grid-cols-3 gap-3">
+          {recipes.map(recipe => (
+            <div key={recipe.id} className="bg-orange rounded-lg shadow-lg overflow-hidden">
+              <img src={`https://spoonacular.com/recipeImages/${recipe.id}-556x370.${recipe.imageType}`} alt={recipe.title} className="w-full h-56 object-cover" />
+              <div className="px-4 py-2">
+                <h2 className="font-semibold text-lg text-orange-700 mb-2">{recipe.title}</h2>
+                <p className="text-gray-700 text-sm">{recipe.summary}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );
